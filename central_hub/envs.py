ca75e1e9213eb1a189e786c524362eb3183d0f8a
Yaroslav Kishchuk <98614059+kshchuk@users.noreply.github.com>
2025-06-14 15:26:00 +0300

    docker update

--- central_hub/envs.py
+++ central_hub/envs.py
@@ -3,8 +3,8 @@ import os
 DB_PATH = "alerts/alertdb.sqlite"
 CAMERA_INDEX = 0
 IMAGE_DIR = "images"
-MQTT_BROKER = "localhost"
-MQTT_PORT = 1883
+MQTT_BROKER = os.getenv("MQTT_BROKER", "localhost")
+MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
 
 CENTRAL_API_URL = os.getenv("CENTRAL_API_URL", "http://localhost:8001")
 HUB_NAME = str(os.getenv("HUB_NAME", "HUB_NAME"))
